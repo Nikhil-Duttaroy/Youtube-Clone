68212c679cbf7da2b9f27367e9eeca9092ff65de
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-07-12 20:35:10 +0530

    sidebar styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { useState } from "react";
 //css imports
-import './_app.scss';
+import "./_app.scss";
 //custom imports
-import Header from './components/Header/Header.component';
-import Sidebar from './components/Sidebar/Sidebar.component';
-import Homepage from './pages/HomePage/HomePage.component';
+import Header from "./components/Header/Header.component";
+import Sidebar from "./components/Sidebar/Sidebar.component";
+import Homepage from "./pages/HomePage/HomePage.component";
 //bootstrap imports
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
+const App = () => {
+  const [sidebar, setSidebar] = useState(false);
 
-const App = () =>  {
+  const handleSidebarToggle = () => setSidebar((value) => !value);
+
   return (
     <>
-      <Header />
-      <div className='app_container border border-primary'>
-        <Sidebar />
-        <Container fluid className='app_main border border-danger'>
+      <Header handleSidebarToggle={handleSidebarToggle} />
+      <div className='app_container '>
+        <Sidebar sidebar={sidebar} handleSidebarToggle={handleSidebarToggle} />
+        <Container fluid className='app_main'>
           <Homepage />
         </Container>
       </div>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import "./Header.styles.scss"
+import {FaBars} from "react-icons/fa"
+import { AiOutlineSearch } from "react-icons/ai";
+import {MdNotifications , MdApps } from "react-icons/md"; // md : material design
 
-const Header = () => {
-    return (
-        <div className="border border-info">
-            Header
-        </div>
-    )
-}
+const Header = ({ handleSidebarToggle }) => {
+  return (
+    <>
+    <div className='header'>
+      <FaBars
+        className='header_menu'
+        sie={26}
+        onClick={() => handleSidebarToggle()}
+      />
+      <img
+        src='https://pngimg.com/uploads/youtube/youtube_PNG2.png'
+        alt='youtube logo'
+        className='header_logo'
+      />
+      <form>
+        <input type='text' placeholder='Search' />
+        <button type='submit'>
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+
+      <div className='header_icons'>
+        <MdNotifications size={27} />
+        <MdApps size={27} />
+        <img
+          src='https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png'
+          alt='avatar'
+        />
+      </div>
+    </div>
+    <hr style={{backgroundColor:"white",margin:"0px"}}/>
+    </>
+  );
+};
 
 export default Header
