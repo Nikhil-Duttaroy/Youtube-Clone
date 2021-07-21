d83b01f3b337833fb37b19a3271a7447af72f5d9
Nikhil Duttaroy <54467400+Nikhil-Duttaroy@users.noreply.github.com>
2021-07-21 21:50:46 +0530

    comments reducer added

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,15 @@
 import { combineReducers } from "redux";
 import { authReducer } from './auth/auth.reducer';
-import { videosReducer } from './videos/videos.reducer';
+import { selectedVideoReducer, videosReducer } from './videos/videos.reducer';
+import { channelDetailsReducer } from './channel/channel.reducers';
+import { commentsListReducer } from './comments/comments.reducer';
 
 const RootReducer = combineReducers({
   auth: authReducer,
-  homeVideos:videosReducer
+  homeVideos: videosReducer,
+  selectedVideo: selectedVideoReducer,
+  channelDetails: channelDetailsReducer,
+  commentsList: commentsListReducer,
 });
 
 export default RootReducer
